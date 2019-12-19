65d5b20072a21803f318bde8de8399383a05a8ba
olympia <owojcik7@gmail.com>
2019-12-19 16:57:17 -0500

    Create the Basic Project Setup

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { FlatList } from "react-native"
+
+const ProductOverviewScreen = props => {
+    return (
+        <View><FlatList /></View>
+    )
+}
+
+export default ProductOverviewScreen

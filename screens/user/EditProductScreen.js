4c2a113719b812e0ff045cbd096f13b67d85d37b
olympia <owojcik7@gmail.com>
2019-12-22 21:53:45 -0500

    Use Params to Submit User Input

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { useSelector } from "react-redux"
 import { View, Text, TextInput, ScrollView, StyleSheet, Platform } from "react-native"
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -15,7 +15,13 @@ const EditProductScreen = props => {
     const [price, setPrice] = useState('')
     const [description, setDescription] = useState(editedProduct ? editedProduct.description : "")
 
+    const submitHandler = useCallback(() => {
+        console.log("submitting")
+    }, [])
 
+    useEffect(() => {
+        props.navigation.setParams({ 'submit': submitHandler })
+    }, [submitHandler])
 
     return (
         <ScrollView>
@@ -44,6 +50,7 @@ const EditProductScreen = props => {
 }
 
 EditProductScreen.navigationOptions = navData => {
+    const submitFn = navData.navigation.getParam('submit')
     return {
         headerTitle: navData.navigation.getParam('productId') ? "Edit Product" : "Add Product",
         headerRight:
@@ -52,9 +59,7 @@ EditProductScreen.navigationOptions = navData => {
                 <Item
                     title="Save"
                     iconName={Platform.OS === "android" ? 'md-checkmark' : 'ios-checkmark'}
-                    onPress={() => {
-                        //submit my form here
-                    }} />
+                    onPress={submitFn} />
             </HeaderButtons>,
     }
 }

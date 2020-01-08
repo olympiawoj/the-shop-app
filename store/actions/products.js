416c3aebb2a2adcf5d8b3d8682927b6f0f5ba33c
olympia <owojcik7@gmail.com>
2020-01-08 10:11:15 -0500

    Update and Delete Products in products action creators

--- store/actions/products.js
+++ store/actions/products.js
@@ -38,9 +38,16 @@ export const fetchProducts = () => {
 }
 
 export const deleteProduct = productId => {
-    return {
-        type: DELETE_PRODUCT,
-        pid: productId
+    return async dispatch => {
+        //any async code you want!
+        await fetch(`https://reactnative-shop-app.firebaseio.com/products/${productId}.json`, {
+            method: 'DELETE'
+        })
+
+        dispatch({
+            type: DELETE_PRODUCT,
+            pid: productId
+        })
     }
 }
 
@@ -78,14 +85,30 @@ export const createProduct = (title, description, imageUrl, price) => {
 }
 
 export const updateProduct = (id, title, description, imageUrl) => {
-    return {
-        type: UPDATE_PRODUCT,
-        pid: id,
-        productData: {
-            //modern JS syntax
-            title,
-            description,
-            imageUrl,
-        }
+    return async dispatch => {
+        //any async code you want!
+        await fetch(`https://reactnative-shop-app.firebaseio.com/products/${id}.json`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl
+            })
+        })
+
+
+        dispatch({
+            type: UPDATE_PRODUCT,
+            pid: id,
+            productData: {
+                //modern JS syntax
+                title,
+                description,
+                imageUrl,
+            }
+        })
     }
 }

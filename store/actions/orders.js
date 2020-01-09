1f6ed64a1bee110b5702768fad2c914ea2a35944
olympia <owojcik7@gmail.com>
2020-01-09 11:56:41 -0500

    Store Orders on Firebase Server by editing logic in orders action creator and reducer

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,35 @@
 export const ADD_ORDER = "ADD_ORDER"
 
 export const addOrder = (cartItems, totalAmount) => {
-    return ({
-        type: ADD_ORDER,
-        orderData: { items: cartItems, amount: totalAmount }
-    })
+    return async dispatch => {
+
+        const date = new Date().toISOString()
+        //any async code you want!
+        const response = await fetch('https://reactnative-shop-app.firebaseio.com/orders/u1.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cartItems,
+                totalAmount,
+                //convert to string save locally in App and save on server
+                date: date
+            })
+        })
+        //get back res data which includes auto generated id 
+        const resData = await response.json()
+        console.log(resData)
+
+        dispatch({
+            type: ADD_ORDER,
+            orderData: { id: resData.name, date: date, items: cartItems, amount: totalAmount }
+        })
+
+        if (!response.ok) {
+            throw new Error("Something went wrong adding an order")
+        }
+
+
+    }
 }
